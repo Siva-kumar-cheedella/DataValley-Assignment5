85bfe9785cb5336ac112ffcae678402affc6320c
Siva-kumar-cheedella <86705861+Siva-kumar-cheedella@users.noreply.github.com>
2024-03-26 15:47:32 +0530

    Update Department.java

--- Department.java
+++ Department.java
@@ -24,23 +24,23 @@ public class Department {
         Department department = new Department(1, "IT");
 
         try {
-            // Loading the JDBC driver
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             
-            // Establishing connection
+            
             String url = "jdbc:mysql://localhost:3306/departments";
             String user = "your_username";
             String password = "your_password";
             Connection connection = DriverManager.getConnection(url, user, password);
             
-            // Creating statement
+            
             Statement statement = connection.createStatement();
             
-            // Inserting department into the database
+            
             String sql = "INSERT INTO department (id, name) VALUES (" + department.getId() + ", '" + department.getName() + "')";
             statement.executeUpdate(sql);
             
-            // Closing statement and connection
+            
             statement.close();
             connection.close();
             
